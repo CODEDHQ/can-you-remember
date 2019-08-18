8baa0875e3359a06356cf7e839cc43c78c8a3ae7
Khalid Al Mohammed <kalmohammed56@gmail.com>
2019-08-18 11:23:22 +0300

    Starting Point

--- src/App.js
+++ src/App.js
@@ -1,37 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-import Home from "./Components/Home";
-import Difficulty from "./Components/Difficulty";
-import Game from "./Components/Game";
 
 function App() {
-  const [mode, setMode] = useState(null);
-  const [difficulty, setDifficulty] = useState(null);
-
-  const page = () => {
-    if (difficulty) return <Game difficulty={difficulty} mode={mode} />;
-    if (mode) return <Difficulty setDifficulty={setDifficulty} />;
-    return <Home setMode={setMode} />;
-  };
-
-  return (
-    <div className="App border my-5">
-      {page()}
-      <div className="row">
-        <div className="col-3">
-          <button
-            className="btn btn-danger mb-3"
-            onClick={() => {
-              setMode(null);
-              setDifficulty(null);
-            }}
-          >
-            Reset
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="App border my-5">Place your code here</div>;
 }
 
 export default App;
